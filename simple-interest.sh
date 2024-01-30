663ff07e50db532e4e7a5641444f66701bfa78a6
waltercvv <53157266+waltercvv@users.noreply.github.com>
2024-01-30 08:15:56 -0500

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,11 @@
+# simple-interest Calculator
+
+## Input import and rate
+import = float(input("Enter the import in Dollars: "))
+rate = float(input("Enter the rate: "))
+
+## Calculate the interest
+interest = import * rate
+
+## Display the result
+print(f"interest is: {interest} USD")
